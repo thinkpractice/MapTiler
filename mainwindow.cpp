df27b2d012aea22e08a32717aeda1e44e67713e7
Tim De Jong <tjadejong@thinkpractice.nl>
2018-01-21 14:04:50 +0100

    Fixed segmentation fault in geo lookup because a constructed QGeoServiceProvider went out of scope. Also added selection signal to geoServiceComboBox.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,70 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QStandardItem>
+#include <iostream>
+
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    _geoServiceProvider = new QGeoServiceProvider("osm");
+    _geoCodingManager = _geoServiceProvider->geocodingManager();
+    connect(_geoCodingManager, SIGNAL(error(QGeoCodeReply*,QGeoCodeReply::Error,QString)), this, SLOT(error(QGeoCodeReply*,QGeoCodeReply::Error,QString)));
+    connect(_geoCodingManager, SIGNAL(finished(QGeoCodeReply*)), this, SLOT(geoLocationFound(QGeoCodeReply*)));
+
+    _geoServiceComboBox = findChild<QComboBox*>("geoServiceComboBox");
+    connect(_geoServiceComboBox, SIGNAL(currentIndexChanged(const QString &)), this, SLOT(comboBoxIndexChanged(const QString&)));
+    _geoServiceComboBox->setModel(createServiceProviderList());
+
+    _addressField = findChild<QLineEdit*>("addressField");
+
+    QPushButton* searchButton = findChild<QPushButton*>("searchButton");
+    connect(searchButton, SIGNAL(clicked(bool)), this, SLOT(searchButtonClicked(bool)));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete _geoServiceProvider;
+}
+
+QStandardItemModel* MainWindow::createServiceProviderList()
+{
+    QStandardItemModel* serviceProviderModel = new QStandardItemModel();
+    QStringList qGeoSrvList = QGeoServiceProvider::availableServiceProviders();
+    for (QString entry : qGeoSrvList)
+    {
+        QStandardItem* item = new QStandardItem(entry);
+        serviceProviderModel->appendRow(item);
+    }
+    return serviceProviderModel;
+}
+
+void MainWindow::searchButtonClicked(bool checked)
+{
+    QString address = _addressField->text();
+    QGeoCodeReply* reply = _geoCodingManager->geocode(address);
+}
+
+void MainWindow::geoLocationFound(QGeoCodeReply* reply)
+{
+    cout << reply->locations().size() << "error=" << reply->errorString().toStdString() << endl;
+    for (auto location : reply->locations())
+    {
+        cout << location.coordinate().latitude() << "," << location.coordinate().longitude() << endl;
+    }
+}
+
+void MainWindow::error(QGeoCodeReply *reply, QGeoCodeReply::Error error, QString errorString)
+{
+    cout << "error=" << errorString.toStdString() << endl;
+}
+
+void MainWindow::comboBoxIndexChanged(const QString &text)
+{
+    cout << text.toStdString() << " selected in combobox" << endl;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QtLocation/QGeoServiceProvider>
+#include <QtLocation/QGeoCodingManager>
+#include <QtLocation/QGeoCodeReply>
+#include <QStandardItemModel>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +21,21 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void searchButtonClicked(bool checked);
+    void geoLocationFound(QGeoCodeReply* reply);
+    void error(QGeoCodeReply *reply, QGeoCodeReply::Error error, QString errorString = QString());
+    void comboBoxIndexChanged(const QString &text);
+
+private:
+    QStandardItemModel* createServiceProviderList();
+
 private:
     Ui::MainWindow *ui;
+    QLineEdit* _addressField;
+    QComboBox* _geoServiceComboBox;
+    QGeoServiceProvider* _geoServiceProvider;
+    QGeoCodingManager* _geoCodingManager;
 };
 
 #endif // MAINWINDOW_H
