1af4d0753cf448b5af887656624e51d73870bc3b
tjadejong <tjadejong@thinkpractice.nl>
2018-01-24 13:51:33 +0100

    Started adding code to access raster data

--- MapViewer.cpp
+++ MapViewer.cpp
@@ -13,5 +13,36 @@ int main()
     if( poDataset == NULL )
     {
         cout << "Error occurred" << endl;
+	return -1;
     }
+
+    cout << "Get first raster band" << endl;
+    GDALRasterBand* poBand = poDataset->GetRasterBand(1);
+
+    cout << "Get BlockSize" << endl;
+    int nXBlockSize, nYBlockSize;
+    poBand->GetBlockSize(&nXBlockSize, &nYBlockSize);
+
+    int nXBlocks = (poBand->GetXSize() + nXBlockSize - 1) / nXBlockSize;
+    int nYBlocks = (poBand->GetYSize() + nYBlockSize - 1) / nYBlockSize;
+    
+    cout << "blocksize (" << nXBlockSize << "," << nYBlockSize << ")";
+    cout << "number of blocks (" << nXBlocks << "," << nYBlocks << ")";		
+
+    for (int y = 0; y < nYBlocks; y++)
+    {
+    	GByte* data = (GByte*)CPLMalloc(nXBlockSize * nYBlockSize);
+  	for (int x = 0; x < nXBlocks; x++)
+        {
+	    CPLErr error = poBand->ReadBlock(x, y, data);
+	    if (error != CPLErr::CE_None)
+		CPLFree(data);
+		continue;
+	    //int nXValid, nYValid;
+	    //poBand->GetActualBlockSize(x, y, &nXValid, &nYValid); 
+        }
+	CPLFree(data);
+    }
+
+    return 0;
 }
