71b1554aca4609412909edd2c62324babbc74b54
tjadejong <tjadejong@thinkpractice.nl>
2018-01-25 11:27:57 +0100

    Get key and value from metadata key,value pairs

--- MapViewer.cpp
+++ MapViewer.cpp
@@ -1,8 +1,29 @@
 #include "gdal_priv.h"
 #include "cpl_conv.h" // for CPLMalloc()
 #include <iostream>
+#include <regex>
+#include <vector>
+#include <string>
 
 using namespace std;
+
+vector<string> splitKeyValuePair(const char* keyValueString)
+{
+    vector <string> matchResults;
+
+    regex re("(\\w+)=(.+)");
+    cmatch matches;
+    
+    if (regex_match(keyValueString, matches, re))
+    {
+        for (const auto& matchResult : matches)
+        {
+            matchResults.push_back(string(matchResult));
+        }
+    }
+
+    return matchResults;
+}
 
 int main()
 {
@@ -13,7 +34,7 @@ int main()
     if( poDataset == NULL )
     {
         cout << "Error occurred" << endl;
-	return -1;
+        return -1;
     }
     cout << "raster count=" << poDataset->GetRasterCount() << endl;
     cout << "layer count=" << poDataset->GetLayerCount() << endl;
@@ -26,16 +47,19 @@ int main()
     if (metadata)
     {
         for (int i = 0; metadata[i]; i++)
-	{
-	   cout << metadata[i] << endl;
-	}
-    }	
+        {
+               cout << metadata[i] << endl;
+               vector<string> matches = splitKeyValuePair(metadata[i]);
+               cout << matches[1] << endl;
+               cout << matches[2] << endl;
+        }
+    }
 
     if (!poBand)
     {
-	cout << "poBand not initialized" << endl;
-	return -1;
-    } 
+        cout << "poBand not initialized" << endl;
+        return -1;
+    }
 
     cout << "Get BlockSize" << endl;
     int nXBlockSize, nYBlockSize;
@@ -50,17 +74,15 @@ int main()
 
     for (int y = 0; y < nYBlocks; y++)
     {
-    	GByte* data = (GByte*)CPLMalloc(nXBlockSize * nYBlockSize);
-  	for (int x = 0; x < nXBlocks; x++)
+        GByte* data = (GByte*)CPLMalloc(nXBlockSize * nYBlockSize);
+        for (int x = 0; x < nXBlocks; x++)
         {
-	    CPLErr error = poBand->ReadBlock(x, y, data);
-	    if (error != CPLErr::CE_None)
-		CPLFree(data);
-		continue;
-	    //int nXValid, nYValid;
-	    //poBand->GetActualBlockSize(x, y, &nXValid, &nYValid); 
+            CPLErr error = poBand->ReadBlock(x, y, data);
+            if (error != CPLErr::CE_None)
+                CPLFree(data);
+                continue;
         }
-	CPLFree(data);
+        CPLFree(data);
     }
 
     return 0;
