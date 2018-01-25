9fc72469c6ae0fe22dc1fa2ba005db3518ac70b9
tjadejong <tjadejong@thinkpractice.nl>
2018-01-25 14:31:10 +0100

    Added code to open GL window

--- MapViewer.cpp
+++ MapViewer.cpp
@@ -4,6 +4,8 @@
 #include <regex>
 #include <vector>
 #include <string>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 using namespace std;
 
@@ -43,6 +45,15 @@ string getKeyType(string key)
     return "";
 }
 
+void printMetadataList(GDALDataset* dataset)
+{
+    char** metadataList = dataset->GetMetadataDomainList();
+    for (int i = 0; metadataList[i]; i++)
+    {
+        cout << metadataList[i] << endl;
+    }
+}
+
 int main()
 {
     GDALAllRegister();
@@ -55,6 +66,7 @@ int main()
         return -1;
     }
 
+    printMetadataList(wmtsDataset);
     char** metadata = wmtsDataset->GetMetadata("SUBDATASETS");
 
     vector<Dataset> datasets;
@@ -101,10 +113,31 @@ int main()
         }
         while (datasetIndex < 0 || datasetIndex >= datasets.size());
         poDataset = (GDALDataset*)GDALOpen(datasets[datasetIndex].url.c_str(), GA_ReadOnly);
+        GDALClose(wmtsDataset);
     }
 
-     
+    printMetadataList(poDataset);
+    char** metadata2 = poDataset->GetMetadata("IMAGE_STRUCTURE");
+    for (int i = 0; metadata2[i]; i++)
+    {
+        cout << metadata2[i] << endl;
+    }
+
+    cout << "===GeoTransform===" << endl;
+    double geoTransform[6];
+    poDataset->GetGeoTransform(geoTransform);
+    for (int i = 0; i < 6; i++)
+    {
+        cout << geoTransform[i] << endl;
+    }
+
+    cout << "===GeoTransform===" << endl;
+
+
+
     cout << "raster count=" << poDataset->GetRasterCount() << endl;
+    cout << "raster X size=" << poDataset->GetRasterXSize() << endl;
+    cout << "raster Y size=" << poDataset->GetRasterYSize() << endl;
     cout << "layer count=" << poDataset->GetLayerCount() << endl;
     cout << "gcp count=" << poDataset->GetGCPCount() << endl;
     cout << "Get first raster band" << endl;
@@ -123,13 +156,13 @@ int main()
 
     cout << "Blocksize (" << nXBlockSize << "," << nYBlockSize << ")" << endl;
 
-    /*cout << "Calculate number of blocks" << endl;
-    int nXBlocks = (poBand->GetXSize() + nXBlockSize - 1) / nXBlockSize;
-    int nYBlocks = (poBand->GetYSize() + nYBlockSize - 1) / nYBlockSize;
+    cout << "Calculate number of blocks" << endl;
+    int nXBlocks = (poDataset->GetRasterXSize() + nXBlockSize - 1) / nXBlockSize;
+    int nYBlocks = (poDataset->GetRasterYSize() + nYBlockSize - 1) / nYBlockSize;
     
     cout << "blocksize (" << nXBlockSize << "," << nYBlockSize << ")";
     cout << "number of blocks (" << nXBlocks << "," << nYBlocks << ")";
-
+    /*
     for (int y = 0; y < nYBlocks; y++)
     {
         GByte* data = (GByte*)CPLMalloc(nXBlockSize * nYBlockSize);
@@ -143,5 +176,51 @@ int main()
         CPLFree(data);
     }*/
 
+    // Initialise GLFW
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL 
+
+    // Open a window and create its OpenGL context
+    GLFWwindow* window; // (In the accompanying source code, this variable is global for simplicity)
+    window = glfwCreateWindow( 1024, 768, "Tutorial 01", NULL, NULL);
+    if( window == NULL )
+    {
+        fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window); // Initialize GLEW
+    glewExperimental=true; // Needed in core profile
+    if (glewInit() != GLEW_OK) 
+    {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        return -1;
+    }
+
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    do
+    {
+        // Draw nothing, see you in tutorial 2 !
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    } // Check if the ESC key was pressed or the window was closed
+    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+    glfwWindowShouldClose(window) == 0 );
+
+    GDALClose(poDataset);
     return 0;
 }
