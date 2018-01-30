4260a54a5e898e83fba8afa9b6b73d3df4d332e5
tjadejong <tjadejong@thinkpractice.nl>
2018-01-30 16:57:59 +0100

    Added some methods to Rect

--- Rect.cpp
+++ Rect.cpp
@@ -4,3 +4,24 @@ Rect::Rect(double x, double y, double width, double height)
         : _x(x), _y(y), _width(width), _height(height)
 {
 }
+
+double Rect::X()
+{
+    return _x;
+}
+
+double Rect::Y()
+{
+    return _y;
+}
+
+double Rect::Width()
+{
+    return _width;
+}
+
+double Rect::Height()
+{
+    return _height;
+}
+
--- Rect.h
+++ Rect.h
@@ -6,6 +6,11 @@ class Rect
     public:
         Rect(double x, double y, double width, double height);
 
+        double X();
+        double Y();
+        double Width();
+        double Height();
+
     private:
         double _x, _y, _width, _height;
 
