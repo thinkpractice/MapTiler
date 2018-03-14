1ffae947394ee3de4494680602626b3e2dbe76d5
tjadejong <tjadejong@thinkpractice.nl>
2018-03-14 13:48:23 +0100

    More work on AreaLookup

--- Lib/AreaLookup.cpp
+++ Lib/AreaLookup.cpp
@@ -1,6 +1,9 @@
 #include "AreaLookup.h"
 #include <QGeoCoordinate>
+#include <QGeoAddress>
 #include <iostream>
+#include <vector>
+#include "Point.h"
 
 using namespace std;
 
@@ -12,6 +15,51 @@ AreaLookup::AreaLookup()
     }
 }
 
+
+vector<string> AreaLookup::ServiceProviders()
+{
+    if (_serviceProviders.size() > 0)
+        return _serviceProviders;
+
+    for (QString entry : QGeoServiceProvider::availableServiceProviders())
+    {
+        QGeoServiceProvider provider(entry);
+        if (provider.geocodingFeatures().testFlag(QGeoServiceProvider::NoGeocodingFeatures))
+            continue;
+        _serviceProviders.push_back(entry.toUtf8().constData());
+    }
+    return _serviceProviders;
+}
+
+void AreaLookup::SetGeoServiceProvider(QGeoServiceProvider* serviceProvider)
+{
+    if (_geoServiceProvider)
+        delete _geoServiceProvider;
+    _geoServiceProvider = serviceProvider;
+    _geoCodingManager = _geoServiceProvider->geocodingManager();
+    connect(_geoCodingManager, SIGNAL(error(QGeoCodeReply*,QGeoCodeReply::Error,QString)), this, SLOT(Error(QGeoCodeReply*,QGeoCodeReply::Error,QString)));
+    connect(_geoCodingManager, SIGNAL(finished(QGeoCodeReply*)), this, SLOT(GeoLocationFound(QGeoCodeReply*)));
+}
+
 Area AreaLookup::GetAreaForAddress(string address)
 {
+}
+
+void AreaLookup::GeoLocationFound(QGeoCodeReply* reply)
+{
+    cout << reply->locations().size() << "error=" << reply->errorString().toStdString() << endl;
+    for (auto location : reply->locations())
+    {
+        QString locationString = QString("%1 - (%2, %3)").arg(location.address().text()).arg(location.coordinate().latitude()).arg(location.coordinate().longitude());
+        cout << "location found:" << locationString.toStdString().c_str() << endl;
+
+        //QGeoCoordinate coordinate = location.coordinate();
+        //QPoint p (coordinate.latitude(), coordinate.longitude());
+
+    }
+}
+
+void AreaLookup::Error(QGeoCodeReply *reply, QGeoCodeReply::Error error, QString errorString)
+{
+    cout << "error=" << errorString.toStdString() << endl;
 }
--- Lib/AreaLookup.h
+++ Lib/AreaLookup.h
@@ -2,19 +2,31 @@
 #define AREA_LOOKUP_H
 
 #include <string>
+#include <vector>
 #include "Area.h"
 
+#include <QObject>
 #include <QGeoServiceProvider>
 #include <QGeoCodingManager>
+#include <QGeoCodeReply>
 
-class AreaLookup
+class AreaLookup : public QObject
 {
     public:
         AreaLookup();
 
         Area GetAreaForAddress(string address);
+        vector<string> ServiceProviders();
 
     private:
+        void SetGeoServiceProvider(QGeoServiceProvider* serviceProvider);
+
+    private slots:
+        void GeoLocationFound(QGeoCodeReply* reply);
+        void Error(QGeoCodeReply *reply, QGeoCodeReply::Error error, QString errorString);
+
+    private:
+        vector<string> _serviceProviders;
         QGeoServiceProvider* _geoServiceProvider;
         QGeoCodingManager* _geoCodingManager;
 };
