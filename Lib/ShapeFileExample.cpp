03058142e6cd06749f5a556356a565a026ee668e
tjadejong <tjadejong@thinkpractice.nl>
2018-03-27 15:27:55 +0200

    Added example to read ShapeFiles

---
+++ Lib/ShapeFileExample.cpp
@@ -0,0 +1,62 @@
+#include "ogrsf_frmts.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+        return -1;
+    const char* filename = argv[1];
+
+    cout << "Opening " << filename << endl;
+
+    GDALAllRegister();
+    GDALDataset       *poDS;
+    poDS = (GDALDataset*) GDALOpenEx( filename, GDAL_OF_VECTOR, NULL, NULL, NULL );
+    if( poDS == NULL )
+    {
+        printf( "Open failed.\n" );
+        exit( 1 );
+    }
+    OGRLayer  *poLayer;
+    cout << "# layers=" << poDS->GetLayerCount() << endl;
+    poLayer = poDS->GetLayer( 0 );
+    cout << "Here poLayer =" << poLayer << endl;
+    OGRFeature *poFeature;
+    poLayer->ResetReading();
+    while( (poFeature = poLayer->GetNextFeature()) != NULL )
+    {
+        OGRFeatureDefn *poFDefn = poLayer->GetLayerDefn();
+        int iField;
+        for( iField = 0; iField < poFDefn->GetFieldCount(); iField++ )
+        {
+            OGRFieldDefn *poFieldDefn = poFDefn->GetFieldDefn( iField );
+            if( poFieldDefn->GetType() == OFTInteger )
+                printf( "%d,", poFeature->GetFieldAsInteger( iField ) );
+            else if( poFieldDefn->GetType() == OFTInteger64 )
+                printf( CPL_FRMT_GIB ",", poFeature->GetFieldAsInteger64( iField ) );
+            else if( poFieldDefn->GetType() == OFTReal )
+                printf( "%.3f,", poFeature->GetFieldAsDouble(iField) );
+            else if( poFieldDefn->GetType() == OFTString )
+                printf( "%s,", poFeature->GetFieldAsString(iField) );
+            else
+                printf( "%s,", poFeature->GetFieldAsString(iField) );
+        }
+        OGRGeometry *poGeometry;
+        poGeometry = poFeature->GetGeometryRef();
+        if( poGeometry != NULL
+                && wkbFlatten(poGeometry->getGeometryType()) == wkbPoint )
+        {
+            OGRPoint *poPoint = (OGRPoint *) poGeometry;
+            printf( "%.3f,%3.f\n", poPoint->getX(), poPoint->getY() );
+        }
+        else
+        {
+            printf( "no point geometry\n" );
+        }
+        OGRFeature::DestroyFeature( poFeature );
+    }
+    GDALClose( poDS );
+}
+
