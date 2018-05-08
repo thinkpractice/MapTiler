9de73cf2f7790185514a5f4bfaa368b2ea90dc12
tjadejong <tjadejong@thinkpractice.nl>
2018-05-08 16:24:48 +0200

    Small changes

--- Lib/Field.cpp
+++ Lib/Field.cpp
@@ -15,6 +15,7 @@ string Field::Name()
     return string(_fieldDefinition->GetNameRef());
 }
 
+//TODO: make value return value with true value type of field
 string Field::Value()
 {
     return _value;
