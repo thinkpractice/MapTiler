7920826a033356e05a061ae8d3aff8133e9864e9
tjadejong <tjadejong@thinkpractice.nl>
2018-06-08 10:32:52 +0200

    Some fixes to GLTest

--- GLTest.cpp
+++ GLTest.cpp
@@ -78,24 +78,21 @@ int main(int argc, char** argv)
     {
         GLuint vao;
         GLuint shaderProgram;
-        SetupPolygonShaders(&vao, &shaderProgram);
         do
         {
-            
-            glDisable(GL_TEXTURE_2D);
-            glBindVertexArray(vao);
-            glUseProgram(shaderProgram);
+
+            SetupPolygonShaders(&vao, &shaderProgram);
             float vertices[] = {-1.0, 1.0,
                                 1.0, 1.0,
-                                1.0, -1.0,
-                                -1.0, -1.0};
+                                1.0, -1.0};
+                                //-1.0, -1.0};
             
             GLuint vbo;
             glGenBuffers(1, &vbo);
             glBindBuffer(GL_ARRAY_BUFFER, vbo);
             glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-            GLuint elements[] = {
+            /*GLuint elements[] = {
                 0, 1, 2,
                 2, 3, 0
             };
@@ -105,13 +102,14 @@ int main(int argc, char** argv)
             glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
             glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
 
-            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);*/
+            glDrawArrays(GL_TRIANGLES, 0, 3);
 
             glfwSwapBuffers(window);
             glfwPollEvents();
 
-            glDeleteBuffers(1, &vbo);
-            glDeleteBuffers(1, &ebo);
+            //glDeleteBuffers(1, &vbo);
+            //glDeleteBuffers(1, &ebo);
         }
         while (glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
             glfwWindowShouldClose(window) == 0);
