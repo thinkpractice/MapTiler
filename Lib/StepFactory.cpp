c6c07a248f6e5a529ce3ea27e36bbed91bc3eb66
tjadejong <tjadejong@thinkpractice.nl>
2018-07-18 17:55:25 +0200

    More work on StepFactory

--- Lib/StepFactory.cpp
+++ Lib/StepFactory.cpp
@@ -1,10 +1,57 @@
 #include "StepFactory.h"
+#include "TileProducerStep.h"
+#include "AddMetadataStep.h"
+#include "TileFilterStep.h"
+#include "TileDownloadStep.h"
+#include "TileGpuTransferStep.h"
+#include "TileWriterStep.h"
 
 StepFactory::StepFactory()
 {
     _steps =
     {
-
+        {
+            "TileProducerStep",
+            std::shared_ptr<ProcessingStep> (const StepSettings& stepSettings)
+            {
+                return std::make_shared<TileProducerStep>(_mainRasterMap, _mainRasterMap->RectForArea(_areaToProcess), _areaToProcess, _settings.tileWidth, _settings.tileHeight))
+            }
+        },
+        {
+            "AddMetadataStep",
+            std::shared_ptr<ProcessingStep> (const StepSettings& stepSettings)
+            {
+                return std::make_shared<AddMetadataStep>(stepSettings.LayerName(), LoadVectorFile(stepSettings));
+            }
+        },
+        {
+            "TileFilterStep",
+            std::shared_ptr<ProcessingStep> (const StepSettings& stepSettings)
+            {
+                return std::make_shared<TileFilterStep>();
+            }
+        },
+        {
+            "TileDownloadStep",
+            std::shared_ptr<ProcessingStep> (const StepSettings& stepSettings)
+            {
+                return std::make_shared<TileDownloadStep>(stepSettings.LayerName(), LoadRasterMap(stepSettings.LayerUrl()));
+            }
+        },
+        {
+            "TileGpuTransferStep",
+            std::shared_ptr<ProcessingStep> (const StepSettings& stepSettings)
+            {
+                return std::make_shared<TileGpuTransferStep>(stepSettings.TileWidth(), stepSettings.TileHeight());
+            }
+        },
+        {
+            "TileWriterStep",
+            std::shared_ptr<ProcessingStep> (const StepSettings& stepSettings)
+            {
+                return std::make_shared<TileWriterStep>(stepSettings.OutputDirectory());
+            }
+        }
     }
 }
 
@@ -14,6 +61,20 @@ StepFactory::~StepFactory()
 
 std::shared_ptr<ProcessingStep> StepFactory::StepFor(const StepSettings &stepSettings)
 {
-
+    for (auto& step : _steps)
+    {
+        if (step.IsStepFor(stepSettings))
+            return step.Create(stepSettings);
+    }
     return nullptr;
+}
+
+std::shared_ptr<GeoMap> StepFactory::LoadRasterMap(std::string rasterUrl)
+{
+
+}
+
+std::shared_ptr<VectorFile> StepFactory::LoadVectorFile(const StepSettings& stepSettings)
+{
+    return std::make_shared<MappedVectorFile>(stepSettings.LayerUrl(), _mainRasterMap->ProjectionReference(), _mainRasterMap->MapTransform()), stepSettings.LayerIndex())
 }
--- Lib/StepFactory.h
+++ Lib/StepFactory.h
@@ -1,11 +1,51 @@
 #ifndef STEPFACTORY_H
 #define STEPFACTORY_H
 
+#include <memory>
+#include <functional>
+#include "StepSettings.h"
 
 class StepFactory
 {
 public:
     StepFactory();
+    virtual ~StepFactory();
+
+    std::shared_ptr<ProcessingStep> StepFor(const StepSettings& stepSettings);
+
+private:
+    class StepCreator
+    {
+    public:
+        using FunctionType = std::function< std::shared_ptr<ProcessingStep>(const StepSettings&) >;
+
+        StepCreator(std::string name, FunctionType createFunction)
+                    :	_name(name),
+                        _createFunction(createFunction)
+        {
+        }
+
+        bool IsStepFor(const StepSettings& stepSettings)
+        {
+            return _name == stepSettings.Name();
+        }
+
+        std::shared_ptr<ProcessingStep> Create(const StepSettings& stepSettings)
+        {
+            return _createFunction(stepSettings);
+        }
+
+    private:
+        std::string _name;
+        FunctionType _createFunction;
+    };
+
+private:
+    std::shared_ptr<GeoMap> LoadRasterMap(std::string rasterUrl);
+    std::shared_ptr<VectorFile> LoadVectorMap(std::string vectorUrl);
+
+private:
+    std::vector< StepCreator > _steps;
 };
 
 #endif // STEPFACTORY_H
