457f8a65a85d156a436631c0c25843e9759035a6
Tim De Jong <tjadejong@thinkpractice.nl>
2018-08-15 21:06:09 +0200

    Added all pipelines and a script to save all tiles with buildings in a separate database table for debugging purposes

---
+++ CreateFilledTiles.sql
@@ -0,0 +1,6 @@
+﻿select * into filled_tiles
+from tiles
+where tile_id in
+(select distinct(ti.tile_id) from areaofinterest as ai
+inner join tiles as ti on ti.area_id = ai.area_id and ai.area_id = 74
+inner join tile_buildings as tb on tb.tile_id = ti.tile_id)
