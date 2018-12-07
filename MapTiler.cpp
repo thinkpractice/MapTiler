73530b47c67807af4f8e43b3b6009a66ed477b81
Tim De Jong <tjadejong@thinkpractice.nl>
2018-12-07 09:33:54 +0100

    Bumped program version

--- MapTiler.cpp
+++ MapTiler.cpp
@@ -76,7 +76,7 @@ int main(int argc, char** argv)
 {
     QCoreApplication app(argc, argv);
     QCoreApplication::setApplicationName("MapTiler");
-    QCoreApplication::setApplicationVersion("0.2");
+    QCoreApplication::setApplicationVersion("0.3");
 
     CommandLineParser parser;
     switch (parser.Parse())
